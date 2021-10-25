1d8cbcd1c63568887e41e77861155a26292b34a1
fairwarning <zhaoguangcan@huawei.com>
2021-10-25 06:30:34 +0000

    fix rnn error and add some npu fused optimizers

--- src/apex/optimizers/npu_fused_adamw.py
+++ src/apex/optimizers/npu_fused_adamw.py
@@ -22,55 +22,58 @@ from torch.optim.optimizer import Optimizer
 
 from ..contrib.combine_tensors import combine_npu
 
-class NpuFusedAdam(Optimizer):
 
-    """Implements Adam algorithm.
+class NpuFusedAdamW(Optimizer):
+    """Implements AdamW algorithm.
 
     Currently NPU-only.  Requires Apex to be installed via
     ``pip install -v --no-cache-dir --global-option="--cpp_ext" --global-option="--npu_float_status" ./``.
 
-    This version of NPU fused Adam implements 1 fusions.
+    This version of NPU fused AdamW implements 1 fusions.
 
       * A combine-tensor apply launch that batches the elementwise updates applied to all the model's parameters
         into one or a few kernel launches.
 
-    :class:`apex.optimizers.NpuFusedAdam` may be used as a drop-in replacement for ``torch.optim.Adam``::
+    :class:`apex.optimizers.NpuFusedAdamW` may be used as a drop-in replacement for ``torch.optim.AdamW``::
 
-        opt = apex.optimizers.NpuFusedAdam(model.parameters(), lr = ....)
+        opt = apex.optimizers.NpuFusedAdamW(model.parameters(), lr = ....)
         ...
         opt.step()
 
-    :class:`apex.optimizers.NpuFusedAdam` should be used with Amp.  Currently, if you wish to use :class:`NpuFusedAdam`
+    :class:`apex.optimizers.FusedAdamW` should be used with Amp.  Currently, if you wish to use :class:`NpuFusedAdamW`
     with Amp, only ``opt_level O1 and O2`` can be choosed::
 
-        opt = apex.optimizers.NpuFusedAdam(model.parameters(), lr = ....)
+        opt = apex.optimizers.NpuFusedAdamW(model.parameters(), lr = ....)
         model, opt = amp.initialize(model, opt, opt_level="O2")
         ...
         opt.step()
 
 
-    Adam was been proposed in `Adam: A Method for Stochastic Optimization`_.
+    The original Adam algorithm was proposed in `Adam: A Method for Stochastic Optimization`_.
+    The AdamW variant was proposed in `Decoupled Weight Decay Regularization`_.
 
     Arguments:
         params (iterable): iterable of parameters to optimize or dicts defining
             parameter groups
-        lr (float, optional, default=1e-3): learning rate
-        betas (Tuple[float, float], optional, default=(0.9, 0.999)): coefficients used for computing
-            running averages of gradient and its square
-        eps (float, optional, default=1e-8): term added to the denominator to improve
+        lr (float, optional, default: 1e-3): learning rate
+        betas (Tuple[float, float], optional, default: (0.9, 0.999)): coefficients used
+            for computing running averages of gradient and its square
+        eps (float, optional, default: 1e-8): term added to the denominator to improve
             numerical stability
-        weight_decay (float, optional, default=0): weight decay (L2 penalty)
-        amsgrad (boolean, optional, default=False): whether to use the AMSGrad variant of this
-            algorithm from the paper `On the Convergence of Adam and Beyond`_
+        weight_decay (float, optional, default: 1e-2): weight decay coefficient
+        amsgrad (boolean, optional, default: False): whether to use the AMSGrad variant of
+            this algorithm from the paper `On the Convergence of Adam and Beyond`_
 
-    .. _Adam - A Method for Stochastic Optimization:
+    .. _Adam\: A Method for Stochastic Optimization:
         https://arxiv.org/abs/1412.6980
+    .. _Decoupled Weight Decay Regularization:
+        https://arxiv.org/abs/1711.05101
     .. _On the Convergence of Adam and Beyond:
         https://openreview.net/forum?id=ryQu7f-RZ
     """
 
     def __init__(self, params, lr=1e-3, betas=(0.9, 0.999), eps=1e-8,
-                 weight_decay=0, amsgrad=False):
+                 weight_decay=1e-2, amsgrad=False):
         if lr < 0.0:
             raise ValueError("Invalid learning rate: {}".format(lr))
         if eps < 0.0:
@@ -84,10 +87,10 @@ class NpuFusedAdam(Optimizer):
         defaults = dict(lr=lr, betas=betas, eps=eps,
                         weight_decay=weight_decay, amsgrad=amsgrad)
         self.is_npu_fused_optimizer = True
-        super(NpuFusedAdam, self).__init__(params, defaults)
+        super(NpuFusedAdamW, self).__init__(params, defaults)
 
     def __setstate__(self, state):
-        super(NpuFusedAdam, self).__setstate__(state)
+        super(NpuFusedAdamW, self).__setstate__(state)
         for group in self.param_groups:
             group.setdefault('amsgrad', False)
 
@@ -136,7 +139,7 @@ class NpuFusedAdam(Optimizer):
                     continue
                 grad = p.grad
                 if grad.is_sparse:
-                    raise RuntimeError('NpuFusedAdam does not support sparse gradients, '
+                    raise RuntimeError('NpuFusedAdamW does not support sparse gradients, '
                                        'please consider SparseAdam instead')
 
                 self._init_param_state(p, amsgrad)
@@ -146,7 +149,7 @@ class NpuFusedAdam(Optimizer):
                 exp_avg_sq_list.append(state['exp_avg_sq'])
                 if amsgrad:
                     max_exp_avg_sq_list.append(state['max_exp_avg_sq'])
-            
+
             combined_step = 0
             combined_exp_avg = None
             combined_exp_avg_sq = None
@@ -157,7 +160,7 @@ class NpuFusedAdam(Optimizer):
                 combined_exp_avg = combine_npu(exp_avg_list)
                 combined_exp_avg_sq = combine_npu(exp_avg_sq_list)
                 combined_max_exp_avg_sq = combine_npu(max_exp_avg_sq_list)
-            
+
             combined_state = defaultdict(dict)
             combined_state['step'] = combined_step
             combined_state['exp_avg'] = combined_exp_avg
@@ -184,10 +187,11 @@ class NpuFusedAdam(Optimizer):
         for p in group['params']:
             if p.grad is None:
                 continue
+
             grad = p.grad
             if grad.is_sparse:
-                raise RuntimeError('NpuFusedAdam does not support sparse gradients, '
-                                    'please consider SparseAdam instead')
+                raise RuntimeError('NpuFusedAdamW does not support sparse gradients, '
+                                   'please consider SparseAdam instead')
             state_p = self.state[p]
             state_p['step'] += 1
 
@@ -199,11 +203,14 @@ class NpuFusedAdam(Optimizer):
         combined_group_grads = stash.combined_grads_indexed_by_group[group_index]
         combined_group_param_states = stash.combined_param_states_indexed_by_group[group_index]
 
-        for combined_param, combined_grad, combined_param_state in zip(combined_group_params, 
-                                                                       combined_group_grads, 
+        for combined_param, combined_grad, combined_param_state in zip(combined_group_params,
+                                                                       combined_group_grads,
                                                                        combined_group_param_states):
             if combined_param is None or combined_grad is None:
                 continue
+
+            # Perform stepweight decay. The fused method is used here to speed up the calculation
+            combined_param.mul_(1 - group['lr'] * group['weight_decay'])
 
             exp_avg, exp_avg_sq = combined_param_state['exp_avg'], combined_param_state['exp_avg_sq']
             if amsgrad:
@@ -212,9 +219,6 @@ class NpuFusedAdam(Optimizer):
             combined_param_state['step'] += 1
             bias_correction1 = 1 - beta1 ** combined_param_state['step']
             bias_correction2 = 1 - beta2 ** combined_param_state['step']
-
-            if group['weight_decay'] != 0:
-                combined_grad = combined_grad.add(combined_param, alpha=group['weight_decay'])
 
             # Decay the first and second moment running average coefficient
             exp_avg.mul_(beta1).add_(combined_grad, alpha=1 - beta1)
@@ -234,7 +238,7 @@ class NpuFusedAdam(Optimizer):
     @torch.no_grad()
     def step(self, closure=None):
         if not hasattr(self, "_amp_stash"):
-            raise RuntimeError('apex.optimizers.NpuFusedAdam should be used with AMP.')
+            raise RuntimeError('apex.optimizers.NpuFusedAdamW should be used with AMP.')
 
         self._check_already_combined_params_and_grads()
         # combine params and grads first
